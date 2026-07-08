0f9de9b71980b6e3fa14ce14ac57a493a3db2616
jaspreet-kalsi <53015619+jaspreet-kalsi@users.noreply.github.com>
2026-07-09 00:44:19 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Ai-Face-Matcher/', // Exact repository name to fix trailing slash 404 issues
+  base: '/Ai-Face-Matcher-v1/', // Matches the exact repository name!
 })
